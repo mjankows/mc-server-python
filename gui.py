7cb53932522f2c3a029b0599c5983c46b8d86328
mjankows <mjankows@hamilton.edu>
2022-06-16 01:46:32 -0400

    very rough first push

---
+++ gui.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+import datetime
+import _thread
+from tkinter import ttk
+from os import system
+from json import loads
+from time import sleep
+
+SERVER_GUI = tk.Tk()
+FRAME = ttk.Frame(SERVER_GUI, padding=500)
+FRAME.grid()
+SERVER_UP = False
+
+def startup_shutdown_loop():
+    global SERVER_UP
+    while True:
+        sleep(1)
+        if datetime.datetime.now().hour < 12  and SERVER_UP:
+            SERVER_UP = False
+            system("stop")
+        elif datetime.datetime.now().hour >=12 and not SERVER_UP:
+            SERVER_UP = True
+            system("java -Xmx1024M -Xmn1024M -jar server.jar nogui")
+        else:
+            print(datetime.datetime.now())
+
+
+def add_command_button(text, command, col, row):
+    ttk.Button(FRAME, text = text, command = lambda: system(command)).grid(
+        column=col, row=row)
+        
+def window_setup(commands):
+    ttk.Label(FRAME, text="Matthew's Minecraft Server Gui").grid(column=0, row=0)
+    for i in commands:
+        for j, item in enumerate(commands[i]):
+            add_command_button(item, commands[i][item],i, j+1)
+    _thread.start_new_thread(startup_shutdown_loop, ())
+    tk.mainloop()
+
+
+
+
+
+
+def main():
+    with open('commands') as f:
+        data = f.read()
+    print(data)
+    data = loads(data)
+    print(data)
+    print(datetime.datetime.now())
+    for i, item in enumerate(data):
+        print(i, data[item])
+    window_setup(data)
+
+
+if __name__ == "__main__":
+    main()
